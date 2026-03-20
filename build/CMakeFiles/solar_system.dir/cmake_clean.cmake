f1b07b8952ed164666ff6fa5d25d2e55ab80ea42
nortcele7 <shreyamregmi5@gmail.com>
2026-03-20 15:30:23 +0545

    Add build

---
+++ build/CMakeFiles/solar_system.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/solar_system.dir/src/main.cpp.o"
+  "CMakeFiles/solar_system.dir/src/main.cpp.o.d"
+  "solar_system"
+  "solar_system.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/solar_system.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
